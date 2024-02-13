417d5b7aafcfe4f8f04b6d06b8e729030acfc479
rktlskan021 <rktlskan021@gmail.com>
2024-02-14 01:10:56 +0900

    Feat: Led connenct

--- py/main.py
+++ py/main.py
@@ -1,33 +1,14 @@
-import RPi.GPIO as GPIO
 import time
 import cv2
 import threading
 from webcam_test import Camera
 from yolov5 import detect
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(22,GPIO.OUT)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(27,GPIO.OUT)
 
-def setLed(n):
-    if n == 0:
-        GPIO.output(17,True)
-        time.sleep(5)
-        GPIO.output(17,False)
-    elif n == 1:
-        GPIO.output(27,True)
-        time.sleep(5)
-        GPIO.output(27,False)
-    elif n == 2:
-        GPIO.output(22,True)
-        time.sleep(5)
-        GPIO.output(22,False)
+def run():
+    ans = detect.run(weights= "py/best.pt", source=0, conf_thres=0.5)
+    
 
 if __name__ == '__main__':
-    # c = Camera()
-    # frame = c.getImage()
-    ans = detect.run(weights= "py/best.pt", source=1)
-    setLed(ans)
-    c.cam.release()
+    ans = detect.run(weights= "py/best.pt", source=0, conf_thres=0.5)
+    
